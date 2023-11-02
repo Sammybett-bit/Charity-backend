c06dfbe7c5e68e4362782648a33d9a3bc8c1cf9e
Sammybett-bit <bettsammy20@gmail.com>
2023-11-02 12:59:33 +0300

    added git ignore

--- app/config.py
+++ app/config.py
@@ -5,4 +5,6 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATION =False
+    #postgres://sammy:9jKGglIRSeWMrXEu0GlkjcbKPS8gsgzX@dpg-cl1mnbg310os73dfttkg-a.oregon-postgres.render.com/charity_udt5
