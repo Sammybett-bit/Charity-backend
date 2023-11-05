052f2945a37b94573d6e1abc22ba3c49a1610026
Sammybett-bit <bettsammy20@gmail.com>
2023-11-05 20:32:23 +0300

    updated all files

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os 
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
 
-    SQLALCHEMY_TRACK_MODIFICATION =False
+    # SQLALCHEMY_TRACK_MODIFICATION =False
     #postgres://sammy:9jKGglIRSeWMrXEu0GlkjcbKPS8gsgzX@dpg-cl1mnbg310os73dfttkg-a.oregon-postgres.render.com/charity_udt5
